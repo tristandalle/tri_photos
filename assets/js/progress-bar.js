ffb6b07d25917a30728c7cee8a6cbfb6ea454abe
tristandalle <tristandalle@gmail.com>
2019-04-02 22:56:48 +0200

    functionnal upload progressbar

--- assets/js/progress-bar.js
+++ assets/js/progress-bar.js
@@ -1,8 +1,15 @@
 var fileInput = document.querySelector('#photo_file'),
-    progress = document.querySelector('#progress');
+    containerProgressBar = $('.progress'),
+    progressBar = $('.progress-bar'),
+    progressPercent = $('.percent-progress');
+    animeCss = $('.lds-roller');
+
+containerProgressBar.hide();
+progressBar.hide();
+progressPercent.hide();
+animeCss.hide();
 
 $('form').submit(function (e) {
-    e.preventDefault();
     var xhr = new XMLHttpRequest();
 
     xhr.open('POST', '/add');
@@ -11,14 +18,25 @@ $('form').submit(function (e) {
         form.append('file', fileInput.files[i]);
     }
 
-    /*console.log(fileInput.files.size);*/
     xhr.upload.addEventListener('progress', function(e) {
-        progress.value = e.loaded;
-        progress.max = e.total;
-        console.log(e);
-        /*if (e.loaded === e.total) {
-            window.location.href = "/photos"
-        }*/
+        loadedValue = e.loaded;
+        totalValue = e.total;
+
+console.log(fileInput.files.length);
+        loadedPercent = (e.loaded * 100) / e.total;
+        containerProgressBar.show();
+        progressBar.show();
+        progressPercent.show();
+        progressBar.width(loadedPercent + '%');
+        progressPercent.html(loadedPercent.toFixed() + ' %');
+        if (e.loaded === e.total) {
+            progressPercent.fadeOut(2000)
+                .queue(function(n) {
+                    $(this).html("transfert terminé ! Vous allez être redirigé vers vos photos");
+                    n();
+                }).fadeIn(500);
+            animeCss.show();
+        }
     });
 
     xhr.addEventListener('load', function (e) {
@@ -35,62 +53,5 @@ $('form').submit(function (e) {
     });*/
 
     xhr.send(form);
-});
-/*
-$('form').submit(function (e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var inputElement = $('#photo_file');
-    var nbFiles = inputElement[0].files.length;
-    var totalSize = 0;
-
-    for (i = 0; i < nbFiles; i++) {
-        totalSize += inputElement[0].files[i].size
-    }
-
-    console.log(inputElement[0].files.length);
-    console.log(totalSize);
-
-    $('#progress-bar').width(0 +'%');
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/add');
-    xhr.onprogress = function(e){
-        var Etotal = e.total;
-        console.log('etotal='+ Etotal);
-        var loaded = Math.round((e.loaded / e.total) * 100);
-        $('#progress-bar').width(loaded + '%');
-        console.log(loaded);
-    };
-*/
-
-    /*xhr.onload = function(){
-        $('#progress-bar').width(100 +'%');
-    }*/
-/*
-
-    formData.append('file', inputElement[0].files);
-    console.log(formData);
-    xhr.send(formData);
 
 });
-
-var progressBar = $('#progress-bar');
-
-$('form').submit(function (e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    $.ajax({
-        url: '/add',
-        xhr: function () {
-            var xhr = new XMLHttpRequest()
-            var total = $('#photo_file').files[0].size;
-            xhr.upload.addEventListener('progress', function (e) {
-                var loaded = Math.round((e.loaded / total) * 100);
-                $('#progress-bar').width(loaded + '%');
-            })
-            return xhr;
-        }
-
-    })
-});
-*/
