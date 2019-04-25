6821de8016c190c3364502d4f8a35d26fd37e8ad
tristandalle <tristandalle@gmail.com>
2019-04-25 12:05:18 +0200

    function add share photo by mail

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\PhotoRepository;
 use App\Repository\UserRepository;
+use Swift_Attachment;
+use Swift_Image;
 use Swift_Mailer;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -35,5 +39,56 @@ class MailerController extends AbstractController
 
         $mailer->send($message);
         return $this->redirectToRoute($returnPath);
+    }
+
+    /**
+     * @Route("/mailer-photo/{from}/{photoId}/{currentPath}", name="mailer_photo")
+     */
+    public function sendPhotoAction($from = null, $photoId = null, $currentPath = null, Request $request, UserRepository $userRepository, PhotoRepository $photoRepository, Swift_Mailer $mailer)
+    {
+        $fromEmail = $userRepository->find($from)->getEmail();
+        $fromName = $userRepository->find($from)->getUsername();
+        $toEmail = $request->request->get('to-email');
+        $content = $request->request->get('content');
+        $fileToSend = $photoRepository->find($photoId)->getFile();
+        $fileName = $photoRepository->find($photoId)->getOriginalName();
+        $albumId = $photoRepository->find($photoId)->getAlbum()->getId();
+        $attachment =  Swift_Attachment::fromPath('C:\wamp64\www\P9\TriPhotos\assets\uploads\images\thumbnails\mini_'.$fileToSend)
+            ->setFilename($fileName)
+            ->setDisposition('inline');
+
+        $message = (new Swift_Message());
+            $cid = $message->embed(Swift_Image::fromPath('C:\wamp64\www\P9\TriPhotos\assets\uploads\images\thumbnails\mini_'.$fileToSend)->setFilename($fileName));
+            $message->setSubject($fromName.' vous envoie une photo via Triphotos');
+            $message->setFrom($fromEmail);
+            $message->setTo($toEmail);
+            $message->setBody(
+                '<html>' .
+                ' <body>' .
+                '  <p>Voici la photo que <strong>' .$fromName. '</strong> vous envoie : <img src="' . $cid . '"/></p>' .
+                ' <i>' . $content . '</i> <p>Vous aussi profitez de vos photos sur <a href="">Triphotos</a></p>' .
+                ' </body>' .
+                '</html>',
+                'text/html'
+            );
+            $message->attach($attachment);
+
+        $mailer->send($message);
+        $this->addFlash(
+            'notice',
+            'Votre message a bien été envoyé à l\'adresse : '.$toEmail
+        );
+        if ($currentPath == 'all-photos') {
+            $returnPath = 'image_all_photos';
+            return $this->redirectToRoute($returnPath);
+        } elseif ($currentPath == 'one-album') {
+            $returnPath = 'album_one_album';
+            return $this->redirectToRoute($returnPath, [
+                "id" => $albumId
+            ]);
+        } else {
+            return $this->redirectToRoute('home_connected');
+        }
+
     }
 }
